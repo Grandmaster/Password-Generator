f6c606dd7d26ed1f431d98f8ad40726b24f33dce
Chidi <cvuwawke@gmail.com>
2020-03-06 12:19:52 -0500

    Added the code that actually generates the password

--- Develop/script.js
+++ Develop/script.js
@@ -8,6 +8,9 @@ function writePassword() {
 
   passwordText.value = password;
 }
+// ===========================================
+// CUSTOM CODE BEGINS
+
 // randomIndex is a function that takes an array/string and generates a random number
 // between 0 and the length of the array/string - 1, to be used to pick out a random
 // member of the array/string.
@@ -25,9 +28,9 @@ function generatePassword() {
       specialCharacters: " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~"
     };
 
-    var passwordLength = prompt(
+    var passwordLength = parseInt(prompt(
       "How long do you want your password to be? (Choose a number between 8 and 128)"
-    );
+    ));
     if (isNaN(parseInt(passwordLength))) {
       alert("That is not a number. Please choose a number");
       continue;
@@ -48,22 +51,34 @@ function generatePassword() {
     );
 
     var criteria = [includeLower, includeUpper, includeNumber, includeSpecial];
-    var validKeys = Object.keys(passwordCharacters);
+    var keys = Object.keys(passwordCharacters);
     // The following loop checks to see which characters the user does not want,
-    // and eliminates the corresponding key from the validKeys array.
+    // and eliminates the corresponding key from the keys array.
     for (var i = 0; i < criteria.length; i++) {
       if (criteria[i] == false) {
-        validKeys = validKeys.slice(0, i).concat(i+1, validKeys.length);
+        keys[i] = "";
       }
     }
+    var validKeys = keys.filter(a => {
+      return a !== "";
+    });
+    // This bit of the code actually generates the password from the Passwordcharacters
+    // object.
     var password = "";
     for (var i = 1; i < passwordLength + 1; i++) {
-      
+      var pickType = validKeys[randomIndex(validKeys)];
+      var pickChar = passwordCharacters[pickType];
+      var Char = pickChar[randomIndex(pickChar)];
+      password = password + Char;
     }
-    break;
+    console.log(password);
+    console.log(password.length);
+    return password;
   }
 }
+// generatePassword();
+// CUSTOM CODE ENDS
+// ==============================
 
-generatePassword();
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
